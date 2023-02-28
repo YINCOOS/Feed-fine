7e35d67def91a53514f95eaead4aacc65a3409a8
Samson Osiomwan <81101034+samsonosiomwan@users.noreply.github.com>
2023-02-28 01:11:40 +0100

    added bottom navigation

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { SafeArea } from './src/components/utility/safe-area.component';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => {
+  return (
+    <SafeArea>
+    <Text>Settings</Text>
+    </SafeArea>
+  );
+}
+
+const Map = () => {
+  return (
+    <SafeArea>
+    <Text>Map</Text>
+    </SafeArea>
+  );
+}
 
 export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
-      <RestaurantsScreen/>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen name="Map" component={Map} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
     </>
 
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import {Searchbar} from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SafeArea } from '../../../components/utility/safe-area.component';
 
-import {
-  StatusBar,
-SafeAreaView,
-  Platform,
-  FlatList
-} from 'react-native';
+import {FlatList } from 'react-native';
 import {RestaurantInfoCard} from '../components/restaurants-info-card.component';
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
